b9824900e0b3a13c6b132e73af4fc8dabfed8146
Sophie Little <119007056+erinsophie@users.noreply.github.com>
2023-03-27 16:59:31 +0100

    Add dark toggle button

---
+++ script.js
@@ -0,0 +1,14 @@
+
+
+const checkbox = document.getElementById('checkbox');
+const header = document.querySelector('.header');
+const sidebar = document.querySelector('.sidebar');
+const mainContent = document.querySelector('.main-content');
+
+
+checkbox.addEventListener('change', () => {
+  header.classList.toggle('dark')
+  sidebar.classList.toggle('dark')
+  mainContent.classList.toggle('darker');
+})
+
