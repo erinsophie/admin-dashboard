fdbbc209d059b62e20c5a632a8fd84fd239593e1
Sophie Little <119007056+erinsophie@users.noreply.github.com>
2023-03-27 17:34:15 +0100

    Create time of day greeting

--- script.js
+++ script.js
@@ -4,8 +4,7 @@ const checkbox = document.getElementById('checkbox');
 const header = document.querySelector('.header');
 const sidebar = document.querySelector('.sidebar');
 const mainContent = document.querySelector('.main-content');
-
-
+const greeting = document.querySelector('.greeting');
 
 
 checkbox.addEventListener('change', () => {
@@ -13,5 +12,20 @@ checkbox.addEventListener('change', () => {
   sidebar.classList.toggle('dark')
   mainContent.classList.toggle('darker');
 })
+
+let hours = new Date().getHours();
+
+let morning = "Good morning,"
+let afternoon = "Good afternoon,"
+let evening = "Good evening,"
+
+if(hours >= 0 && hours < 12) {
+    greeting.textContent = morning
+} else if (hours >= 12 && hours < 17) {
+    greeting.textContent = afternoon
+} else if (hours >= 17 && hours < 24) {
+    greeting.textContent = evening
+}
+
 
 
